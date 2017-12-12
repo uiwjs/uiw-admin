90925eca8328b9370935549df7e6594461161455
jaywcjlove <398188662@qq.com>
2017-12-12 17:35:18 +0800

    Add card.

--- src/routes/Component/AvatarList/index.js
+++ src/routes/Component/AvatarList/index.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
+import { Card } from 'uiw';
 import AvatarList from '../../../components/AvatarList';
 import PageHeaderLayout from '../../../layouts/PageHeaderLayout';
 
 export default () => (
   <PageHeaderLayout title="头像列表" content="一组用户头像，常用在项目/团队成员列表。可通过设置 size 属性来指定头像大小。">
-    <AvatarList size="small">
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
-        console.log(e)
-      }} />
-      <AvatarList.Item />
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
-    </AvatarList>
-    <AvatarList>
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
-        console.log(e)
-      }} />
-      <AvatarList.Item />
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
-    </AvatarList>
-    <AvatarList size="large">
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
-        console.log(e)
-      }} />
-      <AvatarList.Item />
-      <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
-    </AvatarList>
+    <Card title="我的头像" noHover={true}>
+      <AvatarList size="small">
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
+          console.log(e)
+        }} />
+        <AvatarList.Item />
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
+      </AvatarList>
+      <AvatarList>
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
+          console.log(e)
+        }} />
+        <AvatarList.Item />
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
+      </AvatarList>
+      <AvatarList size="large">
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" tips="avatar" onClick={(e) => {
+          console.log(e)
+        }} />
+        <AvatarList.Item />
+        <AvatarList.Item src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
+      </AvatarList>
+    </Card>
   </PageHeaderLayout>
 );
