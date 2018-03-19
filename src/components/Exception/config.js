3a65a48cdf0a543000e3d57f23842f077c406d96
jaywcjlove <398188662@qq.com>
2018-03-19 10:56:08 +0800

    fix(Exception): Image loading error.

--- src/components/Exception/config.js
+++ src/components/Exception/config.js
@@ -1,19 +1,17 @@
 
-import imageURL from './assets/403.svg';
-
 const config = {
   403: {
-    img: `./dist/${imageURL}`,
+    img: require('./assets/403.svg'),
     title: '403',
     desc: '抱歉，你无权访问该页面',
   },
   404: {
-    img: `./dist/${imageURL}`,
+    img: require('./assets/404.svg'),
     title: '404',
     desc: '抱歉，你访问的页面不存在',
   },
   500: {
-    img: `./dist/${imageURL}`,
+    img: require('./assets/500.svg'),
     title: '500',
     desc: '抱歉，服务器出错了',
   },
