b8f0c3c5db4e2908163829703a6621aa80f5ea10
jaywcjlove <398188662@qq.com>
2018-04-05 13:59:45 +0800

    Development mode port collision issue and add default open browser.

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,15 @@
 import webpack from 'webpack';
 import path from 'path';
+import detect from 'detect-port';
+import opn from 'opn';
 import WebpackDevServer from 'webpack-dev-server';
 import apiMocker from 'webpack-api-mocker';
 import load from 'loading-cli';
 import 'colors-cli/toxic';
 import conf from './conf/webpack.config.dev';
+
+let PORT = 19421;
+const HOST = 'localhost';
 
 const compiler = webpack(conf);
 const loading = load('Compiler is running...'.green).start();
@@ -15,23 +20,29 @@ loading.color = 'green';
 compiler.hooks.done.tap('done', () => {
   loading.stop();
   // eslint-disable-next-line
-  console.log(`\nDev Server Listening at ${'http://localhost:19421/'.green}`);
+  console.log(`\nDev Server Listening at ${`http://${HOST}:${PORT}`.green}`);
 });
 
-new WebpackDevServer(compiler, {
-  // contentBase: conf.output.appPublic,
-  publicPath: conf.output.publicPath,
-  hot: true,
-  before(app) {
-    apiMocker(app, path.resolve('./mocker/index.js'), {
-      // 'GET /api/user/list': 'http://localhost:3000',
-    });
-  },
-  historyApiFallback: true,
-  quiet: true,
-}).listen(19421, 'localhost', (err) => {
-  if (err) {
-    // eslint-disable-next-line
-    return console.log(err);
-  }
+detect(PORT).then((_port) => {
+  if (PORT !== _port) PORT = _port;
+  new WebpackDevServer(compiler, {
+    // contentBase: conf.output.appPublic,
+    publicPath: conf.output.publicPath,
+    hot: true,
+    before(app) {
+      apiMocker(app, path.resolve('./mocker/index.js'), {
+        // 'GET /api/user/list': 'http://localhost:3000',
+      });
+    },
+    historyApiFallback: true,
+    quiet: true,
+  }).listen(PORT, HOST, (err) => {
+    if (err) {
+      return console.log(err); // eslint-disable-line
+    }
+    // open browser
+    opn(`http://${HOST}:${PORT}`);
+  });
+}).catch((err) => {
+  console.log(err); // eslint-disable-line
 });
