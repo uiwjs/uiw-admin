adeb45b88d3e515f4b11fdb3a2f379709614748d
yaochuxia <675993729@qq.com>
2019-03-24 22:53:32 +0800

    完善仪表盘页头

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -3,11 +3,15 @@ import styles from './index.module.less';
 
 export default class PageHeader extends PureComponent {
   render() {
-    const { title } = this.props;
+    const { title, content, extraContent } = this.props;
     return (
       <div className={styles.pageHeader}>
         <div className={styles.title}>
           {title}
+        </div>
+        <div className={styles.row}>
+          {content && <div className={styles.content}>{content}</div>}
+          {extraContent && <div className={styles.extraContent}>{extraContent}</div>}
         </div>
       </div>
     );
