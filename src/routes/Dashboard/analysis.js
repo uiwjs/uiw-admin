29f6bfbceee26017ab98f363f3269b50ab22d083
yaochuxia <675993729@qq.com>
2019-03-29 23:10:49 +0800

    图表页面添加模块

--- src/routes/Dashboard/analysis.js
+++ src/routes/Dashboard/analysis.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Button } from 'uiw';
+import { Row, Col, Card, Tooltip, Icon } from 'uiw';
+import styles from './analysis.module.less';
 
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <div>
-        <h1>数据分析</h1>
+      <div className={styles.layout}>
+        <Row gutter={10}>
+          <Col grow={2}>
+            <Card style={{ width: 240 }} bodyStyle={{ padding: 10 }}>
+              <div>
+                <img alt="example" width="100%" src="https://avatars1.githubusercontent.com/u/1680273?v=4" />
+              </div>
+              <div>
+                <h3 style={{ margin: 0 }}>我爱漂亮妹妹</h3>
+                <p style={{ margin: 0 }}><a href="https://uiwjs.github.io">https://uiwjs.github.io</a></p>
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
