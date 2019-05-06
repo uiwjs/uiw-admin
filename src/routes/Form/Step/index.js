8a539098990807dcc0d1b02f504354107350aa38
yaochuxia <675993729@qq.com>
2019-05-06 10:18:16 +0800

    添加步骤表单代码

--- src/routes/Form/Step/index.js
+++ src/routes/Form/Step/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Steps, Form, Input, Select, Row, Col, Button, message, Spin } from 'uiw';
 import PageHeader from '../../../components/PageHeader';
+import styles from './index.module.less';
 
 export default class StepForm extends React.Component {
   constructor(props) {
@@ -17,7 +19,13 @@ export default class StepForm extends React.Component {
       <div>
         <PageHeader
           title="分步表单"
+          content="将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成"
         />
+        <div className={styles.stepForm}>
+          <div className={styles.stepBox}>
+
+          </div>
+        </div>
       </div>
     );
   }
