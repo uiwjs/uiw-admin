81f16adfa936c58c6bb9e6eea92025448c6114a8
SunLxy <SunLxy@users.noreply.github.com>
2022-01-14 22:23:44 +0000

    deploy: 2910520414df0bac51d8f76fb64285adb6c659b3

--- src/models/index.tsx
+++
@@ -1,2 +0,0 @@
-
-export * from "@uiw-admin/models"
--- src/pages/TableList/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {ProTable } from '@uiw-admin/components'
-
-export default function index() {
-  return (
-    <ProTable />
-  )
-}
