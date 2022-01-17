06d4661fc8081cfe0187eb01533753bbf5ef3599
SunLxy <SunLxy@users.noreply.github.com>
2022-01-17 06:37:04 +0000

    deploy: 0850e5549d0a66b390796fdc8a4ff30b5930a0a9

--- 953.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_examples_base=self.webpackChunk_examples_base||[]).push([[953],{9953:function(e,t,n){n.r(t);var a=n(4795),r=n(3028),s=n(7162),u=n.n(s),c={name:"doc",state:{test:"doc model"},reducers:{updateState:function(e,t){return(0,r.Z)((0,r.Z)({},e),t)}},effects:function(e){return{verify:function(){return(0,a.Z)(u().mark((function t(){return u().wrap((function(t){for(;;)switch(t.prev=t.next){case 0:e.global.updateState({test:"\u6d4b\u8bd52"});case 2:case"end":return t.stop()}}),t)})))()}}}};t.default=c}}]);
-//# sourceMappingURL=953.js.map
