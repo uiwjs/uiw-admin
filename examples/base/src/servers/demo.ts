895f9ca40200b76b89ebd012ee25008574de229a
WX <42308734@qq.com>
2022-01-17 17:40:24 +0800

    fix(example):demo 使用swr 模拟新增编辑api

--- examples/base/src/servers/demo.ts
+++ examples/base/src/servers/demo.ts
@@ -1,30 +1,5 @@
 import { request } from '@uiw-admin/utils';
 
-
-// /api/demo/selectPage
-export function selectDemoPage(params: any) {
-  return request('/api/demo/selectPage', {
-    method: 'POST',
-    body: { ...params },
-  });
-}
-
-// /api/demo/insert
-export function insert(params: any) {
-  return request('/api/demo/insert', {
-    method: 'POST',
-    body: { ...params },
-  });
-}
-
-// /api/demo/edit
-export function update(params: any) {
-  return request('/api/demo/update', {
-    method: 'POST',
-    body: { ...params },
-  });
-}
-
 // /api/demo/selectById
 export function selectById(params: { id: string }) {
   return request('/api/demo/selectById', {
