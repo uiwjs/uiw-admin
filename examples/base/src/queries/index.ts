3e8c6b6dc90274a6c7fbdf14feb065720a82145a
xingyuefeng <15262870437@163.com>
2022-01-19 17:06:04 +0800

    feat doc: 增加文件目录说明

--- examples/base/src/queries/index.ts
+++ examples/base/src/queries/index.ts
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 
 // 公共接口数据 重复数据hooks 都建在此文件夹
+// 利用swr复用接口数据，各个页面都可用
 // https://swr.vercel.app/zh-CN/docs/getting-started#%E5%8F%AF%E5%A4%8D%E7%94%A8%E7%BB%84%E4%BB%B6
 
 
