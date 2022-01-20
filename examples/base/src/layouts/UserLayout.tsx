9099527dd6a66b6dabcd5e43721be7c4c62bcc0a
SunLxy <1011771396@qq.com>
2022-01-20 15:53:20 +0800

    fix:修改样式

--- examples/base/src/layouts/UserLayout.tsx
+++ examples/base/src/layouts/UserLayout.tsx
@@ -5,19 +5,17 @@ import { Notify } from "uiw"
 const UserLayout = () => {
   const navigate = useNavigate()
 
-  return <div>
-    <UserLogin
-      btnProps={{ type: "primary" }}
-      onSuccess={(data) => {
-        if (data && data.token) {
-          sessionStorage.setItem("token", data.token)
-          sessionStorage.setItem("auth", JSON.stringify(data.authList || []))
-          navigate("/home", { replace: true })
-        } else {
-          Notify.error({ title: "错误通知", description: data.message || "请求失败" })
-        }
-      }}
-    />
-  </div>
+  return <UserLogin
+    btnProps={{ type: "primary" }}
+    onSuccess={(data) => {
+      if (data && data.token) {
+        sessionStorage.setItem("token", data.token)
+        sessionStorage.setItem("auth", JSON.stringify(data.authList || []))
+        navigate("/home", { replace: true })
+      } else {
+        Notify.error({ title: "错误通知", description: data.message || "请求失败" })
+      }
+    }}
+  />
 }
 export default UserLayout;
