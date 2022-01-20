383af3aeb782c721350267bd860c6170b59fae82
jaywcjlove <398188662@qq.com>
2022-01-20 16:30:24 +0800

    chore: update README.md

--- examples/website/src/pages/quick-start/index.tsx
+++ examples/website/src/pages/quick-start/index.tsx
@@ -1,7 +1,7 @@
 import Markdown from '../../components/Markdown';
 
 export default class Page extends Markdown {
-  editorUrl = '/packages/canvas-layer/README.md';
+  editorUrl = '/packages/uiw-admin/README.md';
   // dependencies = { };
   getMdStr = () => import('uiw-admin/README.md');
 }
