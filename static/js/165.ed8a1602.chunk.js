fc8114b699bed105d462df4312c7237a5a69555c
xingyuefeng <xingyuefeng@users.noreply.github.com>
2022-01-25 07:57:14 +0000

    deploy: 1713d7b9df838acc834ca00d7bd1cd3453d0437b

---
+++ static/js/165.ed8a1602.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[165],{57165:function(n,e,s){s.r(e),e.default="# `components`\n\n> \u4e1a\u52a1\u5e38\u7528\u7ec4\u4ef6\n\n## Usage\n\n```\nimport * from '@uiw-admin/components';\n\n\n```\n"}}]);
+//# sourceMappingURL=165.ed8a1602.chunk.js.map
