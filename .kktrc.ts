7bde0897840c88adce8def8093c65c30e2745eb4
SunLxy <SunLxy@users.noreply.github.com>
2022-02-09 01:30:52 +0000

    deploy: 5e3567686043fde0b12f85f85eb2d0d519964895

--- .kktrc.ts
+++ .kktrc.ts
@@ -1,26 +1,32 @@
 import path from 'path'
 import { MockerAPIOptions } from 'kkt'
 import defaultConfig from '@uiw-admin/config'
+import express from 'express'
 
-// import { RematchWebpackPlugin, RoutesWebpackPlugin } from '@uiw-admin/plugins'
 export default defaultConfig({
   define: {
     AUTH: false,
     STORAGE: 'local',
   },
+  // 第一种
+  proxySetup: (app: express.Application): MockerAPIOptions => {
+    return {
+      path: path.resolve('./mocker/index.js'),
+    }
+  },
 })
-
-export const proxySetup = (): MockerAPIOptions => {
-  /**
-   * mocker-api that creates mocks for REST APIs.
-   * It will be helpful when you try to test your application without the actual REST API server.
-   * https://github.com/jaywcjlove/mocker-api
-   */
-  return {
-    path: path.resolve('./mocker/index.js'),
-    /**
-     * https://github.com/jaywcjlove/mocker-api/tree/96c2eb94694571e0e3003e6ad9ce1c809499f577#options
-     */
-    option: {},
-  }
-}
+// 第二种
+// export const proxySetup = (): MockerAPIOptions => {
+//   /**
+//    * mocker-api that creates mocks for REST APIs.
+//    * It will be helpful when you try to test your application without the actual REST API server.
+//    * https://github.com/jaywcjlove/mocker-api
+//    */
+//   return {
+//     path: path.resolve('./mocker/index.js'),
+//     /**
+//      * https://github.com/jaywcjlove/mocker-api/tree/96c2eb94694571e0e3003e6ad9ce1c809499f577#options
+//      */
+//     option: {},
+//   }
+// }
