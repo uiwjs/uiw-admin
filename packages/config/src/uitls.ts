d7c1ed65931ef62b906d3df79a9b0ded59a304de
SunLxy <1011771396@qq.com>
2022-02-11 16:51:30 +0800

    fix:config包define转换

--- packages/config/src/uitls.ts
+++ packages/config/src/uitls.ts
@@ -1,11 +1,7 @@
 export const transformationDefineString = (obj: Record<string, any>) => {
   const result: Record<string, any> = {};
   Object.entries(obj).forEach(([key, value]) => {
-    if (typeof value !== 'string') {
-      result[key] = JSON.stringify(value);
-    } else {
-      result[key] = value;
-    }
+    result[key] = JSON.stringify(value);
   });
   return result;
 };
