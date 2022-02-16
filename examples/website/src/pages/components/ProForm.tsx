6662f39f1e4f4cb2b5f5b15f0b3d71334600183f
WX <42308734@qq.com>
2022-02-16 13:50:43 +0800

    docs(ProForm):文档更新

--- examples/website/src/pages/components/ProForm.tsx
+++ examples/website/src/pages/components/ProForm.tsx
@@ -1,9 +1,17 @@
 import Markdown from '../../components/Markdown';
 import { ProForm, useForm } from '@uiw-admin/components';
-import { Button, Notify, Slider, formatter } from 'uiw';
+import { Button, Notify, Slider, formatter, Card } from 'uiw';
 
 export default class Page extends Markdown {
   editorUrl = '/packages/components/src/ProForm/README.md';
-  dependencies: any = { ProForm, Button, Notify, Slider, formatter, useForm };
+  dependencies: any = {
+    ProForm,
+    Button,
+    Notify,
+    Slider,
+    formatter,
+    useForm,
+    Card,
+  };
   getMdStr = () => import('@uiw-admin/components/src/ProForm/README.md');
 }
