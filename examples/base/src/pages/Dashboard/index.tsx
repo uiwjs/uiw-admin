ff6683cf3268ab0b0dbf4bf0d23a342b39480078
xingyuefeng <15262870437@163.com>
2022-03-01 14:24:02 +0800

    demo: 优化demo，简化显示

--- examples/base/src/pages/Dashboard/index.tsx
+++ examples/base/src/pages/Dashboard/index.tsx
@@ -1,38 +1,6 @@
 import React from 'react'
-import { Button } from 'uiw'
-import { useNavigate } from 'react-router-dom'
-import { useSWRConfig } from 'swr'
-import { Dispatch, RootState } from '@uiw-admin/models'
-import { useSelector, useDispatch } from 'react-redux'
 
 const Dashboard = () => {
-  const navigate = useNavigate()
-  // const [state, setState] = React.useState('')
-
-  const dispatch = useDispatch<Dispatch>()
-
-  const { cache } = useSWRConfig()
-  const stores = useSelector((state: RootState) => state)
-  console.log(stores)
-  console.log('cache', cache.get('login'))
-
-  return (
-    <div>
-      Dashboard
-      <input
-        value={stores.docDs.test}
-        onChange={(event) => {
-          dispatch({
-            type: 'docDs/updateState',
-            payload: {
-              test: event.target.value,
-            },
-          })
-        }}
-      />
-      <hr />
-      <Button onClick={() => navigate('/', { replace: true })}>Logout</Button>
-    </div>
-  )
+  return <div>Dashboard</div>
 }
 export default Dashboard
