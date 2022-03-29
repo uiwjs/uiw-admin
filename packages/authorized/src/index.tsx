265a6f8ca889fda22c8ac2c50125d45c4c4c513c
SunLxy <1011771396@qq.com>
2022-03-29 11:14:01 +0800

    fix(@uiw-admin/authorized):修复AuthPage hook问题

--- packages/authorized/src/index.tsx
+++ packages/authorized/src/index.tsx
@@ -13,17 +13,20 @@ const AuthPage = (
   props: AuthorizedProps = { children: <React.Fragment /> },
 ): JSX.Element => {
   let navigate = useNavigate();
+  /** Navigate 重定向 会报错  */
+  React.useEffect(() => {
+    if (!props.authority) {
+      if (props.redirectPath) {
+        navigate(props.redirectPath, { replace: true });
+      } else {
+        navigate('/login', { replace: true });
+      }
+    }
+  }, [props.redirectPath]);
+
   if (props.authority) {
     return props.children;
   }
-  /** Navigate 重定向 会报错  */
-  React.useEffect(() => {
-    if (props.redirectPath) {
-      navigate(props.redirectPath, { replace: true });
-    } else {
-      navigate('/login', { replace: true });
-    }
-  }, [props.redirectPath]);
   return (
     <React.Suspense fallback={<span>Loading...</span>}>
       <React.Fragment />
