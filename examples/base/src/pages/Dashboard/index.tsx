63f751efa2b1d20b60dd2b39839921130effa9ca
SunLxy <1011771396@qq.com>
2022-04-18 18:29:16 +0800

    fix(basic-layouts):修复菜单参数展示问题

--- examples/base/src/pages/Dashboard/index.tsx
+++ examples/base/src/pages/Dashboard/index.tsx
@@ -1,4 +1,10 @@
+import { navigate } from '@uiw-admin/router-control'
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      Dashboard
+      <button onClick={() => navigate('/tableList/12')}>点击</button>
+    </div>
+  )
 }
 export default Dashboard
--- packages/basic-layouts/src/Breadcrumb/index.tsx
+++ packages/basic-layouts/src/Breadcrumb/index.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { Breadcrumb } from 'uiw';
 import { useLocation } from 'react-router-dom';
 import { BreadcrumbMap } from './../utils';
+import { matchPath } from 'react-router';
+
 interface BreadProps {
   routeMap: BreadcrumbMap;
 }
 const Bread = (props: BreadProps) => {
   const { routeMap } = props;
   const location = useLocation();
-  const domList = routeMap.breadcrumb.get(location.pathname) || [];
+
+  const domList = React.useMemo(() => {
+    const result = routeMap.breadcrumb.get(location.pathname);
+    if (!result) {
+      let paths;
+      routeMap.breadcrumb.forEach((_, key) => {
+        if (matchPath(key, location.pathname)) {
+          paths = key;
+        }
+      });
+      if (paths) {
+        return routeMap.breadcrumb.get(paths) || [];
+      }
+      return [];
+    }
+    return result;
+  }, [location.pathname]);
+
   return (
     <Breadcrumb>
       {domList?.map((item, index) => {
