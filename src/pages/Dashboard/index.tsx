30bb56204dbe0028f49ff1d2b41ab1a9c52ccaea
SunLxy <SunLxy@users.noreply.github.com>
2022-04-21 04:29:12 +0000

    deploy: 531ba6de8a51e5324695ac02be5aded97ff083c7

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,26 @@
 import { navigate } from '@uiw-admin/router-control'
+import { Form } from '@uiw-admin/components'
+import { Input } from 'uiw'
 const Dashboard = () => {
   return (
     <div>
+      <Form
+        fields={{
+          name: {
+            label: '姓名',
+            children: <Input placeholder="请输入姓名" />,
+            rules: [
+              {
+                required: true,
+                message: '必填',
+              },
+              () => {
+                return '222'
+              },
+            ],
+          },
+        }}
+      />
       Dashboard
       <button onClick={() => navigate('/tableList/12')}>点击</button>
       <button onClick={() => navigate('/dom/12')}>点击2</button>
