ab2075520784b953b5916e5f0d38461278c87dc4
姚宾 <yaob421123@163.com>
2023-04-19 13:39:08 +0800

    fix(bas): 调整预览网站路由及新增高级表单展示

---
+++ examples/base/src/pages/Form/models/index.ts
@@ -0,0 +1,27 @@
+import { Dispatch } from '@kkt/pro'
+
+export interface GlobalState {
+  test: string
+  [s: string]: any
+}
+
+const doc = {
+  name: 'form',
+  state: {
+    test: 'doc model',
+  },
+  reducers: {
+    updateState: (state: GlobalState, payload: GlobalState) => ({
+      ...state,
+      ...payload,
+    }),
+  },
+  effects: (dispatch: any) => ({
+    async verify() {
+      const dph = dispatch as Dispatch
+      dph.doc.updateState({ test: '测试2' })
+    },
+  }),
+}
+
+export default doc
