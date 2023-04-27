5bf42fa0eb399d6de8536b5717e34c68b554e4da
姚宾 <yaob421123@163.com>
2023-04-27 16:27:26 +0800

    doc(Login): 调整文档展示

--- examples/website/src/components/Layouts/index/index.tsx
+++ examples/website/src/components/Layouts/index/index.tsx
@@ -1,12 +1,27 @@
 import { Outlet, KktproPageProps } from '@kkt/pro';
 import Menu from '../Menu';
 import Navbar from '../Navbar';
+import { createGlobalStyle } from 'styled-components';
 import { Wrapper, Main, Body } from './style';
 import '@wcj/dark-mode';
+
+export const GlobalStyle = createGlobalStyle`
+  [data-color-mode*='dark'], [data-color-mode*='dark'] body {
+    --gradient-from: #1c1e20;
+    --gradient-to: #0d1117;
+    --color-rgb: 255 255 255;
+  }
+  [data-color-mode*='light'], [data-color-mode*='light'] body {
+    --gradient-from: #e5eaf0;
+    --gradient-to: #fff;
+    --color-rgb: 0 0 0;
+  }
+`;
 
 export default function Layout(props: KktproPageProps) {
   return (
     <Wrapper className="wmde-markdown-var">
+      <GlobalStyle />
       <Navbar />
       <Main>
         <Body>
---
+++ examples/website/src/pages/user-login/Examples.tsx
@@ -0,0 +1,109 @@
+import LoginBase from '@react-login-page/base';
+import LoginLogo from 'react-login-page/logo-rect';
+import LoginPage1 from '@react-login-page/page1';
+import LoginPage2 from '@react-login-page/page2';
+// import bannerImage  from '@react-login-page/page2/banner-image';
+import LoginPage3 from '@react-login-page/page3';
+// import bannerImage3 from '@react-login-page/page3/banner-image';
+import Login4 from '@react-login-page/page4';
+import Login5 from '@react-login-page/page5';
+import Login6 from '@react-login-page/page6';
+import Login7 from '@react-login-page/page7';
+
+const bannerImage = require('@react-login-page/page2/banner-image');
+const bannerImage3 = require('@react-login-page/page3/banner-image');
+
+interface Example {
+  magnify?: number;
+  children?: JSX.Element;
+}
+
+export const base: Example = {
+  magnify: 1.3,
+  children: (
+    <LoginBase>
+      <LoginBase.Logo>
+        <LoginLogo />
+      </LoginBase.Logo>
+    </LoginBase>
+  ),
+};
+
+export const page1: Example = {
+  children: (
+    <LoginPage1>
+      <LoginPage1.Logo>
+        <LoginLogo />
+      </LoginPage1.Logo>
+    </LoginPage1>
+  ),
+};
+
+export const page2: Example = {
+  magnify: 3.3,
+  children: (
+    <LoginPage2>
+      <LoginPage2.Banner>
+        <img src={bannerImage} alt="banner" />
+      </LoginPage2.Banner>
+      <LoginPage2.Logo>
+        <LoginLogo />
+      </LoginPage2.Logo>
+    </LoginPage2>
+  ),
+};
+
+export const page3: Example = {
+  children: (
+    <LoginPage3>
+      <LoginPage3.Banner style={{ backgroundImage: `url(${bannerImage3})` }} />
+      <LoginPage3.Logo>
+        <LoginLogo />
+      </LoginPage3.Logo>
+    </LoginPage3>
+  ),
+};
+
+export const page4: Example = {
+  magnify: 2.3,
+  children: (
+    <Login4>
+      <Login4.Logo>
+        <LoginLogo />
+      </Login4.Logo>
+    </Login4>
+  ),
+};
+
+export const page5: Example = {
+  magnify: 1.6,
+  children: (
+    <Login5>
+      <Login5.Logo>
+        <LoginLogo />
+      </Login5.Logo>
+    </Login5>
+  ),
+};
+
+export const page6: Example = {
+  magnify: 2.1,
+  children: (
+    <Login6>
+      <Login6.Logo>
+        <LoginLogo />
+      </Login6.Logo>
+    </Login6>
+  ),
+};
+
+export const page7: Example = {
+  magnify: 1.2,
+  children: (
+    <Login7>
+      <Login7.Logo>
+        <LoginLogo />
+      </Login7.Logo>
+    </Login7>
+  ),
+};
